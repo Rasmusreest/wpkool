faae9c09eef0795e8db2132ee6ef212d49a21c13
rasmusreestiktkh <rasmus.reest@khk.ee>
2021-02-17 15:53:59 +0200

    modified file `wflogs` (after activation of `Wordfence Security` version 7.4.14)

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1614174834;s:7:"authKey";s:64:">7)|,{r&xqj@O2KR*msZ3vg:u#XvZy27vAsw:a+he*1Y5!EXi|/z*PqJ$hIsP>ZF";}
